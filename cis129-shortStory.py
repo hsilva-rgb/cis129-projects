0b60924059a22f309cd81454a92fa17361e24ec2
hsilva-rgb <hsilva5@mail.pima.edu>
2024-09-25 07:02:10 -0700

    added a short story cis129-shortStory.py
    also removed Hello World!... I'm not sure if my dogs ate that too.

--- cis129-shortStory.py
+++ cis129-shortStory.py
@@ -1,1 +1,9 @@
-print('Hello World!')
+#Hector Silva
+#This is my first python lab in my cis129 course it explains why the file name is slightly incorrect
+#deleted Hello World! script and added the story
+
+print('I have two small dogs named Lucky and Lukey')
+print('They are both less than a year old so they're pretty rambunctious')
+print('They still chew on things they are not supposed to chew on, like pillows, cushions, paper, and wood like on the baseboards and doors')
+print('I thought since my classes are all online that they wouldn't be able to eat my homework but I think they ate the _lab02_ part of this filename')
+print('This is my way of telling you that my dogs ate part of my homework')

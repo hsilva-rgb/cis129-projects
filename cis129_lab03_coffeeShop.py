95a4e091e0c1a898ec1f78f39e01486d13e7632a
hsilva-rgb <hsilva5@mail.pima.edu>
2024-10-03 22:51:27 -0700

    Updated cis129_lab03_coffeeShop.py
    added some options for the cafe and fixed some typos

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -6,23 +6,41 @@
 #text based Coffee Shop simulator that calculates a receipt based on /
 #how many items the user wants to order.
 
+#10/03/2024 some typos removed and 2 items added to the menu
+
 print('welcome to the module 3 cafe!')
 
-#numC stands for the number of coffees bought
-numC= int(input('How many coffees would you like to buy?'))
-#numM stands for the number of muffins bought
-numM= int(input('and how many muffins would you like to buy?'))
+#numC stands for the number of coffees ordered by the user
+numC = int(input('How many coffees would you like to buy?'))
+#numM stands for the number of muffins ordered by the user
+numM = int(input('How many muffins would you like to buy?'))
+#numB stands for the number of brownies ordered by the user
+numB = int(input('How many brownies would you like to buy?'))
+#numF stands for the number of fruit cups ordered by the user
+numF = int(input('and how many fruit cups would you like to buy?'))
 
-#the next steps create variables for the price of one coffee /
-#and one muffin then calculate the total cost of the users order without ta
+#the next steps create variables for the price of one coffee, /
+#one muffin, one brownie, and one fruit cup then calculate the total cost /
+#of the users order without tax
 pcoffee = int(5)
-pmuffin = int(8)
-taxless_total = int((pcoffee * numC) + (pmuffin * numM))
+pmuffin = int(4)
+pbrownie = float(3.5)
+pfruitcup = float(3)
+taxless_total = int((pcoffee * numC) + (pmuffin * numM) + (pbrownie * numB)/
+                    (pfruitcup *numF))
 
 #the next step creates the variable Total and the sales tax variable sales_tax/
 #then adds tax to the taxless_total
-sales_tax = float(1.06)
-Total = sales_tax * taxless_total
-
-print('Thank you for your order. your total is $',Total)
-print('We hope you come again soon!.')
+sales_tax = float(0.06*taxless_total)
+Total = float((sales_tax + taxless_total))
+round(Total, 2)
+round(sales_tax, 2)
+print('Module 3 cafe reciept')
+print(numC, ' cups of coffee')
+print(numM, ' muffin(s)')
+print(numB, 'brownie(s)')
+print(numF, 'fruit cup(s)')
+print('sales tax at 6%', sales_tax)
+print('Your total is $',Total)
+print('Thank you for your order.')
+print('We hope you come again soon!')

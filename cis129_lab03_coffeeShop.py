04452b00e2a5cb3c48b55bffc5d265dc242d6bf9
hsilva-rgb <hsilva5@mail.pima.edu>
2024-10-03 22:01:04 -0700

    Update cis129_lab03_coffeeShop.py

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -1,23 +1,28 @@
 #Hector Silva
-# this script is for the module 3 lab and it creates an interactive text based Coffee Shop simulator that calculates a receipt based on how many items the user wants to order.
+#contact info
+#10/03/2024
 
-print('welcome to the module 3 cafe!)
+#this script is for the module 3 lab and it creates an interactive/
+#text based Coffee Shop simulator that calculates a receipt based on /
+#how many items the user wants to order.
+
+print('welcome to the module 3 cafe!')
 
 #numC stands for the number of coffees bought
-numC= input('How many coffees would you like to buy?)
-
+numC= int(input('How many coffees would you like to buy?'))
 #numM stands for the number of muffins bought
-numM= input('and how many muffins would you like to buy?')
+numM= int(input('and how many muffins would you like to buy?'))
 
-#the next steps create variables for the price of one coffee and one muffin then calculate the total cost of the users order without tax.
+#the next steps create variables for the price of one coffee /
+#and one muffin then calculate the total cost of the users order without ta
+pcoffee = int(5)
+pmuffin = int(8)
+taxless_total = int((pcoffee * numC) + (pmuffin * numM))
 
-pcoffee=5
-pmuffin=8
-taxless_total=pcoffee*numC+pmuffin*numM
+#the next step creates the variable Total and the sales tax variable sales_tax/
+#then adds tax to the taxless_total
+sales_tax = float(1.06)
+Total = sales_tax * taxless_total
 
-#the next step creates the variable Total and the sales tax variable sales_tax then adds tax to the taxless_total
-sales_tax=0.06
-Total=(sales_tax+1)*taxless_total
-
-print('Thank you for your order. your total is $', Total, 'your order will be ready soon.'/n 'Thank you for visiting the module 3 lab cafe!')
+print('Thank you for your order. your total is $',Total)
 print('We hope you come again soon!.')
